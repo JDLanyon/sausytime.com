67455cce91ecab559b4ae333ee3938062049cf5e
JDLanyon <jlany9@hotmail.com>
2024-05-29 06:23:51 +1000

    I actually hate next.js
    added 3 images on landing page but they work like shit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
+import Panel from "./Panel"
 
 export default function Home() {
   return (
-    <main>
-      <h1>im fat</h1>
-    </main>
+    <div className={styles.container}>
+      {/* TODO: add props to Panel (background image and title) */}
+      {/* TODO: style */}
+      {/* TODO: reactive styling */}
+      <Panel title="1" imgsrc='1.jpg'/>
+      <Panel title="2" imgsrc='2.jpg'/>
+      <Panel title="3" imgsrc='3.jpg'/>
+    </div>
   )
 }

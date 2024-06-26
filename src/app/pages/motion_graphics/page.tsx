0c44ae3abf4e7876b1bcbb3dc598f1ab1424525f
JDLanyon <jlany9@hotmail.com>
2024-06-27 00:13:02 +1000

    changed to vite + lit with TS & Sass

--- src/app/pages/motion_graphics/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function MotionGraphics() {
-  return (
-    <div>
-      <h1>This page is about motion graphics</h1>
-    </div>
-  )
-}
--- src/app/pages/programming/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Programming() {
-  return (
-    <div>
-      <h1>This is the page about programming</h1>
-    </div>
-  )
-}

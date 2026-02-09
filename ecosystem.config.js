e3dcea603e18080b5f439f5db7e291a571f5768f
JDLanyon <jlany9@hotmail.com>
2026-02-09 19:14:40 +1000

    ecosystem & eslint config change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,13 +8,14 @@ module.exports = {
       key  : 'key.pub',
       user : 'jdl',
       host : '192.168.0.69',
-      ref  : 'origin/master',
-      repo : 'git@git.fennec.town/JDLanyon/jdlanyon-dev.git',
-      path : '/var/www',
+      // host : '167.179.163.255',
+      ref  : 'origin/main',
+      repo : 'git@git.fennec.town:JDLanyon/jdlanyon-dev.git',
+      path : '/var/www/jdlanyon',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
-      'ssh_options': 'ForwardAgent=yes'
+      'ssh_options': ['ForwardAgent=yes', 'IdentitiesOnly=yes', 'IdentityFile=~/.ssh/id_ed25519']
     }
   }
 };
